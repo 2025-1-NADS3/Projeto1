65fd688673ed82b9d8d42523bc6c4c3786fc1f24
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-03-17 12:48:17 -0300

    Subindo Tela de Cadastro

--- src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/RegisterActivity.java
+++ src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/RegisterActivity.java
@@ -8,42 +8,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button btnRegister = findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        ImageView imgVoltar = (ImageView) findViewById(R.id.imgVoltar);
+        imgVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, WelcomeActivity.class);
                 startActivity(intent);
             }
         });
-
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
