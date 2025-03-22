7709896e274f56aacd78e3915d38e4a3dafd3d06
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-03-22 16:00:09 -0300

    Update WelcomeActivity.java

--- src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/WelcomeActivity.java
+++ src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/WelcomeActivity.java
@@ -2,11 +2,8 @@ package br.com.neonpay.neonpayacademy;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
