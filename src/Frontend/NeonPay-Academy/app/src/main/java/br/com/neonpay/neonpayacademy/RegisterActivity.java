0e1805c61001aba0f13b56a706e107c86a47140a
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-03-22 16:00:19 -0300

    Update RegisterActivity.java

--- src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/RegisterActivity.java
+++ src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/RegisterActivity.java
@@ -9,7 +9,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
