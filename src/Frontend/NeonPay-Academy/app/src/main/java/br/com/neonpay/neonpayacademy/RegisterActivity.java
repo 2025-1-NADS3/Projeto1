9c9b1ec9fadd2966835c35c661784b15f004602d
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-03-22 15:57:31 -0300

    Ajustes na tela de cadastro

--- src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/RegisterActivity.java
+++ src/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/RegisterActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -22,8 +21,6 @@ import org.json.JSONObject;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
