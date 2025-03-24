76202443ecf1b906647946d81c9af8d2fc05f3f6
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-03-23 23:36:39 -0300

    Cria rota para exclusão de perfil

--- src/Backend/routes/userRoutes.js
+++ src/Backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { login, register } = require('../controllers/userController');
 const { atualizarPerfil } = require('../controllers/userController');
+const { deletarPerfil } = require('../controllers/userController');
 const autenticarToken = require('../middlewares/autenticarToken');
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/cadastro', register);
 router.put('/atualizar-perfil', autenticarToken, atualizarPerfil);
+router.delete('/deletar-perfil', autenticarToken, deletarPerfil);
 
 module.exports = router;
