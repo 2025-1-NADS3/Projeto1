530ac539628f66bf5b0ed1a3f67f30c16f08ebf1
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-03-24 12:30:15 -0300

    Criando rota para puxar dados do perfil

--- src/Backend/routes/userRoutes.js
+++ src/Backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { login, register } = require('../controllers/userController');
 const { atualizarPerfil } = require('../controllers/userController');
 const { deletarPerfil } = require('../controllers/userController');
+const { getPerfil } = require('../controllers/userController');
 const autenticarToken = require('../middlewares/autenticarToken');
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.post('/login', login);
 router.post('/cadastro', register);
 router.put('/atualizar-perfil', autenticarToken, atualizarPerfil);
 router.delete('/deletar-perfil', autenticarToken, deletarPerfil);
+router.get('/perfil', autenticarToken, getPerfil)
 
 module.exports = router;
