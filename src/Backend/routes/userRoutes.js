98f7e708542dd42ca27e24863090830a726347c2
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-04-17 21:07:01 -0300

    Atualizando userRoutes.js na troca de callbacks para promises e CommonJS paara ESM

--- src/Backend/routes/userRoutes.js
+++ src/Backend/routes/userRoutes.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const { login, register } = require('../controllers/userController');
-const { atualizarPerfil } = require('../controllers/userController');
-const { deletarPerfil } = require('../controllers/userController');
-const { getPerfil } = require('../controllers/userController');
-const autenticarToken = require('../middlewares/autenticarToken');
+import express from 'express';
+import { login, register, atualizarPerfil, deletarPerfil, getPerfil } from '../controllers/userController.js';
+import autenticarToken from '../middlewares/autenticarToken.js';
+
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/cadastro', register);
 router.put('/atualizar-perfil', autenticarToken, atualizarPerfil);
 router.delete('/deletar-perfil', autenticarToken, deletarPerfil);
-router.get('/perfil', autenticarToken, getPerfil)
+router.get('/perfil', autenticarToken, getPerfil);
 
-module.exports = router;
+export default router;
