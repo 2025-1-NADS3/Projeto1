677d9de0b273d7f2faa45f4d74fa1280059e7200
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-04-17 21:06:40 -0300

    Atualizando autenticarToken.js na troca de callbacks para promises e CommonJS paara ESM

--- src/Backend/middlewares/autenticarToken.js
+++ src/Backend/middlewares/autenticarToken.js
@@ -1,22 +1,24 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const JWT_SECRET = process.env.JWT_SECRET; 
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import dotenv from 'dotenv';
 
-function autenticarToken(req, res, next) {
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const verifyAsync = promisify(jwt.verify);
+
+export default async function autenticarToken(req, res, next) {
     const token = req.headers['authorization']?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ erro: 'Token não fornecido' });
     }
 
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ erro: 'Token inválido ou expirado. Faça login novamente.' });
-        }
-
-        req.user = decoded; 
+    try {
+        const decoded = await verifyAsync(token, JWT_SECRET);
+        req.user = decoded;
         next();
-    });
+    } catch (err) {
+        return res.status(401).json({ erro: 'Token inválido ou expirado. Faça login novamente.' });
+    }
 }
-
-module.exports = autenticarToken;
