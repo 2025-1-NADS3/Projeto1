7d617e446695e400dab994c028b6ff1dec98c759
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-04-18 12:56:11 -0300

    Update db.js

--- src/Backend/config/db.js
+++ src/Backend/config/db.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: false }
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default db;
