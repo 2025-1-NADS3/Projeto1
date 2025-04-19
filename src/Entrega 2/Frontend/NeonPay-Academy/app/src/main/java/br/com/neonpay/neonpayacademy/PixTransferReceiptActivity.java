917341470c3af66dd01bd687ce532bf9dfc5183f
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-04-19 11:18:22 -0300

    Update PixTransferReceiptActivity.java

--- src/Entrega 2/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/PixTransferReceiptActivity.java
+++ src/Entrega 2/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/PixTransferReceiptActivity.java
@@ -1,6 +1,10 @@
 package br.com.neonpay.neonpayacademy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PixTransferReceiptActivity extends AppCompatActivity {
+
+    // Declaração das variáveis dos elementos da interface
+    private ImageView imgVoltar;
+    private double valor;
+    private TextView txtValor;
+    private Button btnMenu;
+    private TextView txtNomeDestinatario;
+    private String nomeDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,33 @@ public class PixTransferReceiptActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Inicialização dos componentes da interface
+        imgVoltar = findViewById(R.id.imgVoltar);
+        txtValor = findViewById(R.id.txtValor);
+        txtNomeDestinatario = findViewById(R.id.txtNomeDestinatario);
+        btnMenu = findViewById(R.id.btnMenu);
+
+        // Recebendo os dados da Tela que foram enviados pelo Intent:
+        Bundle bundle = getIntent().getExtras();
+        valor = bundle.getDouble("valor", 0);
+        nomeDestinatario = bundle.getString("nome_destinatario");
+
+        // Mostrando dados na tela
+        txtValor.setText(String.format("R$ %.2f", valor));
+        txtNomeDestinatario.setText(nomeDestinatario);
+
+        // Botão para retornar à ConfirmPixTransferActivity
+        imgVoltar.setOnClickListener(view -> {
+            finish();
+        });
+
+        btnMenu.setOnClickListener(v -> {
+            // Vai para a tela Home
+            Intent intent = new Intent(PixTransferReceiptActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
     }
 }
