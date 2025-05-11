6d3cb69f0b5a1d8b50433a8a1503ee10bfbdf61a
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-05-11 18:45:32 -0300

    Atualizando rota de extrato

--- src/Entrega 2/Backend/routes/pixRoutes.js
+++ src/Entrega 2/Backend/routes/pixRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { gerarCobranca, webhook, enviarPix, consultarSaldo, consultarPontos, extratoPix } from '../controllers/pixController.js';
+import { gerarCobranca, webhook, enviarPix, consultarSaldo, consultarPontos, extrato } from '../controllers/pixController.js';
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.post("/webhook", webhook);
 router.post("/enviar", enviarPix);
 router.get("/saldo/:id", consultarSaldo);
 router.get("/pontos/:id", consultarPontos);
-router.get("/extrato/:usuario_id", extratoPix);
+router.get("/extrato/:usuario_id", extrato);
 
 export default router;
