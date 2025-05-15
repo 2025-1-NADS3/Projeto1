310614cf29862a6596b917b9722908be0b637149
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-05-15 11:37:54 -0300

    Tela de comprovante de adicionar dinheiro

--- src/Entrega 3/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/PixAddAmountReceiptActivity.java
+++ src/Entrega 3/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/PixAddAmountReceiptActivity.java
@@ -1,6 +1,10 @@
 package br.com.neonpay.neonpayacademy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class PixAddAmountReceiptActivity extends AppCompatActivity {
+
+    // Declaração das variáveis dos elementos da interface
+    private ImageView imgVoltar;
+    private TextView txtValor;
+    private double valor;
+    private Button btnMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,31 @@ public class PixAddAmountReceiptActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Inicialização dos componentes da interface
+        imgVoltar = findViewById(R.id.imgVoltar);
+        txtValor = findViewById(R.id.txtValor);
+        btnMenu = findViewById(R.id.btnMenu);
+
+        // Recebendo os dados da Tela que foram enviados pelo Intent:
+        Bundle bundle = getIntent().getExtras();
+        valor = bundle.getDouble("valor", 0);
+
+        // Mostrando dados na tela
+        NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("pt", "BR")); // Formata o valor para o formato em Reais
+        txtValor.setText(format.format(valor));
+
+        // Botão para retornar à ConfirmPixTransferActivity
+        imgVoltar.setOnClickListener(view -> {
+            finish();
+        });
+
+        btnMenu.setOnClickListener(v -> {
+            // Vai para a tela Home
+            Intent intent = new Intent(PixAddAmountReceiptActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
     }
 }
