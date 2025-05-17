71cf73a8d8df3b75eedecada0891960fc4519e3f
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-05-16 21:39:44 -0300

    Criando tela para servicos do ASA

---
+++ src/Entrega 3/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/AsaIntroActivity.java
@@ -0,0 +1,41 @@
+package br.com.neonpay.neonpayacademy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AsaIntroActivity extends AppCompatActivity {
+
+    // Declaração das variáveis dos elementos da interface
+    private Button btnAvancar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_asa_intro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Inicialização dos componentes da interface
+        btnAvancar = findViewById(R.id.btnAvancar);
+
+        // Quando o botão btnAvancar for clicado, vai para tela de serviços do Asa
+        btnAvancar.setOnClickListener(v -> {
+            Intent intent = new Intent(AsaIntroActivity.this, AsaServiceActivity.class);
+            startActivity(intent);
+        });
+    }
+}
