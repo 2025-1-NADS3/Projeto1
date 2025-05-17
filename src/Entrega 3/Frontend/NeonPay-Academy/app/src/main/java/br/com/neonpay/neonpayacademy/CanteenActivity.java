f979720d90a17b0200f85c298f252b02cad2715f
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-05-17 15:06:45 -0300

    Criando telas para cantina

---
+++ src/Entrega 3/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/CanteenActivity.java
@@ -0,0 +1,125 @@
+package br.com.neonpay.neonpayacademy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.neonpay.neonpayacademy.adapter.CanteenAdapter;
+import br.com.neonpay.neonpayacademy.adapter.ServicoAdapter;
+import br.com.neonpay.neonpayacademy.model.Canteen;
+import br.com.neonpay.neonpayacademy.model.Servico;
+
+public class CanteenActivity extends AppCompatActivity {
+
+    // Declaração das variáveis dos elementos da interface
+    private RecyclerView recyclerCantina;
+    private CanteenAdapter adapter;
+    private List<Canteen> listaCantina;
+    private List<Canteen> listaCarrinho = new ArrayList<>();
+    private ImageView imgVoltar;
+    private ImageView imgCarrinho;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_canteen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Inicialização dos componentes da interface
+        imgVoltar = findViewById(R.id.imgVoltar);
+        recyclerCantina = findViewById(R.id.recyclerCantina);
+        recyclerCantina.setLayoutManager(new GridLayoutManager(this, 2));
+        imgCarrinho = findViewById(R.id.imgCarrinho);
+
+        // Inicialização da lista de itens da cantina e carrega os dados via API
+        listaCantina = new ArrayList<>();
+        carregarItensCantina();
+
+        // Configura o adapter com a lista de serviços e o evento de compra
+        adapter = new CanteenAdapter(this, listaCantina, this::onComprarClick);
+        recyclerCantina.setAdapter(adapter);
+
+        // Botão para retornar à HomeActivity
+        imgVoltar.setOnClickListener(view -> {
+            // Invoca a "HomeActivity"
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        // Cliando no icone de carrinho, leva para a tela de Carrinho (CanteenCartActivity)
+        imgCarrinho.setOnClickListener(v -> {
+            Intent intent = new Intent(CanteenActivity.this, CanteenCartActivity.class);
+            intent.putExtra("carrinho", new ArrayList<>(listaCarrinho));
+            startActivity(intent);
+        });
+
+    }
+
+    // Função para quando o item é comprado
+    public void onComprarClick(Canteen canteen) {
+        listaCarrinho.add(canteen);
+        Toast.makeText(this, canteen.getNome() + " adicionado ao carrinho!", Toast.LENGTH_SHORT).show();
+    }
+
+    // Método para carregar itens da cantina atraves da api /cantina/listarCantina
+    private void carregarItensCantina() {
+        String url = "http://10.0.2.2:3000/cantina/listarCantina";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Criando requisição GET para o servidor
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    listaCantina.clear();
+                    try {
+                        for (int i = 0; i < response.length(); i++) {
+                            // Recebe os objetos retornados pela API, atribui eles a variaveis e os adiciona à lista
+                            JSONObject obj = response.getJSONObject(i);
+                            String nome = obj.getString("titulo");
+                            String descricao = obj.getString("descricao");
+                            int id = obj.getInt("id");
+
+                            // Formata o preço
+                            DecimalFormat df = new DecimalFormat("R$#.00");
+                            String preco = df.format(obj.getDouble("preco"));
+
+                            String imagem = obj.getString("imagem");
+                            listaCantina.add(new Canteen(id, nome, preco, descricao, imagem)); // Adiciona item à lista
+                        }
+                        adapter.notifyDataSetChanged();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> error.printStackTrace()
+        );
+
+        queue.add(request);
+    }
+}
---
+++ src/Entrega 3/Frontend/NeonPay-Academy/app/src/main/java/br/com/neonpay/neonpayacademy/CanteenConfirmPasswordActivity.java
@@ -0,0 +1,224 @@
+package br.com.neonpay.neonpayacademy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.neonpay.neonpayacademy.utils.SharedPrefsHelper;
+
+public class CanteenConfirmPasswordActivity extends AppCompatActivity {
+
+    // Declaração das variáveis dos elementos da interface
+    private ImageView imgVoltar;
+    private EditText txtSenha;
+    private String chave_pix;
+    private int idUsuario;
+    private Button btnConfirmarPagamento;
+    private String nome, email, cpf , token;
+    private ArrayList<Integer> itensIds;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_canteen_confirm_password);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Inicialização dos componentes da interface
+        imgVoltar = findViewById(R.id.imgVoltar);
+        txtSenha = findViewById(R.id.txtSenha);
+        btnConfirmarPagamento = findViewById(R.id.btnConfirmarPagamento);
+
+        // Recupera o token
+        token = SharedPrefsHelper.getToken(this);
+
+        // Verificação se o token é null, se for voltar para tela de login
+        if (token == null) {
+            Toast.makeText(this, "Token não encontrado. Faça login novamente.", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
+
+        // Chama o metodo para buscar dados do usuario
+        buscarDadosUsuario();
+
+        // Recebendo os dados da Tela que foram enviados pelo Intent:
+        Bundle bundle = getIntent().getExtras();
+        chave_pix = bundle.getString("chave_pix");
+
+        // Recuperar lista de IDs dos itens
+        itensIds = bundle.getIntegerArrayList("itens_ids");
+
+        // Recupera o ID do usuário a partir do SharedPrefsHelper
+        idUsuario = SharedPrefsHelper.getUsuarioId(this);
+
+        // Botão para retornar à tela anterior
+        imgVoltar.setOnClickListener(view -> {
+            finish();
+        });
+
+        // Quando o botão btnConfirmarPagamento for clicado, verifica se o campo de senha está preenchido
+        btnConfirmarPagamento.setOnClickListener(v -> {
+            String senha = txtSenha.getText().toString().trim();
+            if (senha.isEmpty()) {
+                Toast.makeText(this, "Digite sua senha", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Chama a função confirmar pagamento do itens da cantina
+            confirmarPagamentoCantina(chave_pix, senha, itensIds);
+        });
+
+    }
+
+    // Função para enviar uma requisição POST para a rota /cantina/pagarCantina, confirmando o pagamento
+    private void confirmarPagamentoCantina(String chavePix, String senha, ArrayList<Integer> itensIds) {
+        String url = "http://10.0.2.2:3000/cantina/pagarCantina";
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("usuario_id", idUsuario);
+            json.put("chave_pix", chavePix);
+            json.put("senha", senha);
+
+            JSONArray itensArray = new JSONArray();
+            for (Integer id : itensIds) {
+                itensArray.put(id);
+            }
+            json.put("itens_ids", itensArray);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Criando requisição POST para o servidor
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, json,
+                response -> {
+                    try {
+                        String mensagem = response.getString("mensagem");
+                        String descricao = response.getString("descricao");
+                        int valorTotal = response.getInt("valor_total");
+                        int senhaPedido = response.getInt("senha_pedido");
+                        String data = response.getString("data");
+                        String chave_pix = response.getString("chave_pix");
+                        JSONArray itensArrayResponse = response.getJSONArray("itens");
+
+                        // Envia todos os dados via Intent
+                        Intent intent = new Intent(CanteenConfirmPasswordActivity.this, CanteenReceiptActivity.class);
+                        intent.putExtra("nome", nome);
+                        intent.putExtra("email", email);
+                        intent.putExtra("senha_pedido", senhaPedido);
+                        intent.putExtra("cpf", mascararCpf(cpf));
+                        intent.putExtra("mensagem", mensagem);
+                        intent.putExtra("descricao", descricao);
+                        intent.putExtra("valor_total", valorTotal);
+                        intent.putExtra("data", data);
+                        intent.putExtra("chave_pix", chave_pix);
+                        intent.putExtra("itens", itensArrayResponse.toString());
+
+                        startActivity(intent);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(this, "Erro ao processar resposta da API.", Toast.LENGTH_SHORT).show();
+                    }
+
+                },
+                error -> {
+                    error.printStackTrace();
+                    Toast.makeText(this, "Erro ao confirmar pagamento", Toast.LENGTH_SHORT).show();
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    // Função para consultar os dados do usuario
+    private void buscarDadosUsuario() {
+        String url = "http://10.0.2.2:3000/api/perfil";
+
+        // Criando requisição GET para o servidor
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        nome = response.getString("nome");
+                        cpf = response.getString("cpf");
+                        email = response.getString("email");
+
+                        if (!validarCPF(cpf)) {
+                            Toast.makeText(this, "CPF retornado inválido.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(this, "Erro ao processar dados do usuário.", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                error -> {
+                    error.printStackTrace();
+                    Toast.makeText(this, "Erro ao buscar dados do perfil.", Toast.LENGTH_SHORT).show();
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    // Função para mascarar o cpf
+    private String mascararCpf(String cpf) {
+        // Valida se o CPF tem o formato correto (11 dígitos)
+        if (cpf != null && cpf.length() == 11) {
+            return cpf.substring(0, 3) + ".***.***-" + cpf.substring(9);
+        }
+        return cpf; // Caso não seja um CPF válido, retorna o CPF original
+    }
+
+    // Função para validar cpf
+    private boolean validarCPF(String cpf) {
+        return cpf != null && cpf.length() == 11 && !cpf.matches("(\\d)\\1{10}");
+    }
+
+}
